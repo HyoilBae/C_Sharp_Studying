e856013ca7c4788fc0d6c0c3986983d9b3739a8d
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-01-29 21:34:43 -0800

    HelloWorld

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string Str = "Hello World";
+            int StrCount = Str.Length;
+            Console.WriteLine(StrCount);
+            Console.WriteLine("This is the number of this sentence");
+        }
+    }
+}
