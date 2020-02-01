ac4b51c297a539ca6ef14000a1f219ddc587c3be
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-01 08:25:41 -0800

    2020-02-01

---
+++ 5-4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nArray1 = { 1, 2, 3, 4, 5 };
+            Array.Clear(nArray1, 2, 3);
+            foreach (int m in nArray1)
+            {
+                Console.Write(m);
+            }
+            Console.WriteLine();
+
+            Array.Clear(nArray1, 0, nArray1.Length);
+            foreach (int m in nArray1)
+            {
+                Console.Write(m);
+            }
+        }
+    }
+}
