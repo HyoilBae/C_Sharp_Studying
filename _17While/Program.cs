9a5343622adb73c4a0a02a67a88479379f6842ca
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-02 07:41:31 -0800

    2020_02_02

---
+++ _17While/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _17While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool Check = true;
+            while (Check)
+            {
+                Console.WriteLine("infinite");
+                Check = false;
+            }
+
+            int CountWhile = 0;
+            while (CountWhile <= 5)
+            {
+                Console.WriteLine(CountWhile);
+                CountWhile = CountWhile + 1;
+            }
+
+            Check = false;
+            do
+            {
+                Console.WriteLine("infinite");
+            } while (Check);
+
+            int Count = 0;
+            do
+            {
+                Count = Count + 1;
+                Console.WriteLine(Count);
+
+            } while (Count < 100);
+        }
+    }
+}
