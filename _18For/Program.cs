23ff641f8ab63b76b62a9ed83bd1630a0d77f177
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-02 07:43:30 -0800

    2020_02_02

---
+++ _18For/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _18For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while 하고 for 는 함수 안에서만 쓸 수 있다
+
+            
+            //for (초기화문; 조건문; 증감문) for 문의 장점은 접근성
+            //{
+                //실행코드
+            //}
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            //이렇게 쓰면 While 이 된다
+            for (; true;)
+            {
+                Console.WriteLine(100);
+            }
+        }
+    }
+}
