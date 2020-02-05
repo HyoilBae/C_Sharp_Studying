f59da2724cd907e66bdc8d4b42c723dbfccedd35
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-05 07:41:21 -0800

    2020_02_05

---
+++ _28Partial/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _28Partial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
