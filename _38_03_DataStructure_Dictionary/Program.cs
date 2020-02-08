ab74743cf0f683eec83865d5bb41259ac6a8a7fe
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-08 13:33:22 -0800

    2020_02_08

---
+++ _38_03_DataStructure_Dictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _38_03_DataStructure_Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> NewDic = new Dictionary<string, int>();
+
+            NewDic.Add("number 1", 1);
+            NewDic.Add("number 2", 2);
+            NewDic.Add("what number is this?", 877978);
+
+            Console.WriteLine(NewDic["number 1"]);
+
+            string Key = "it doens't exist";
+
+            if (true == NewDic.ContainsKey(Key))
+            {
+                Console.WriteLine(NewDic[Key]);
+            }
+
+        }
+    }
+}
