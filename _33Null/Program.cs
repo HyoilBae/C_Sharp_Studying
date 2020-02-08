a3f81eff987417ff7cf284897fb8bd2360ae1ae5
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-08 13:29:54 -0800

    2020_02_08

---
+++ _33Null/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Player
+{
+    private int mAT;
+
+    public int AT
+    {
+        get { return mAT; }
+        set { mAT = value; }
+    }
+}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player NewPlayer = new Player();
+
+        //가르키는게 없이 비워놓겠다
+        //참조형의 데이터 구조를 가리키는 자료형 클래스는 new 하게 되면
+            Player NewPlayer2 = null;
+
+        Console.WriteLine(NewPlayer2.AT);
+
+        //what's "NullREferenceException?
+        Console.WriteLine(NewPlayer.AT);
+
+    }
+}
