cc6304f7fa19e07fd79e48904945ecf5e59c6791
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-09 21:44:53 -0800

    2020_02_09

---
+++ LinkedStructure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkedStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> LList = new LinkedList<int>();
+        }
+    }
+}
