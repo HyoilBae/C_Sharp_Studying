226ad30ecb3ac8503ff822e2da23a068919a24f5
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-11 10:56:27 -0800

    2020_02_11

---
+++ 6-1-3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _6_1_3
+{
+    struct Data
+    {
+        public int var1;
+        public float var2;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Data[] DataArray = new Data[2];
+
+            DataArray[0].var1 = 7;
+            DataArray[0].var2 = 3.14f;
+
+            DataArray[1].var1 = 12;
+            DataArray[1].var2 = 0.5f;
+
+            BinaryWriter bw = new BinaryWriter(File.Open("test.txt", FileMode.Create));
+            for (int i = 0; i < DataArray.Length; i++)
+            {
+                bw.Write(DataArray[i].var1);
+                bw.Write(DataArray[i].var2);
+            }
+            bw.Close();
+
+
+
+            int var1;
+            float var2;
+
+            BinaryReader br = new BinaryReader(File.Open("test.txt", FileMode.Open));
+            while (true)
+            {
+                try
+                {
+                    var1 = br.ReadInt32();
+                    var2 = br.ReadSingle();
+                    Console.WriteLine("{0} {1}", var1, var2);
+                }
+                catch (EndOfStreamException e)
+                {
+                    br.Close();
+                    break;
+                }
+            }
+        }
+    }
+}
