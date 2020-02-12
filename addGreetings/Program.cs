d4a2b6e988bb0fe97cf2e32c53d5daf9e1b22118
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-12 07:30:19 -0800

    2020_02_12

---
+++ addGreetings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addGreetings
+{
+    class Solution
+    {
+        public void AddGreetings(string[] names)
+        {
+            foreach (string name in names)
+            {
+                Console.WriteLine("Hello " + name);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            string[] Names = { "Owen", "Max", "Sophie" };
+            s.AddGreetings(Names);
+        }
+    }
+}
