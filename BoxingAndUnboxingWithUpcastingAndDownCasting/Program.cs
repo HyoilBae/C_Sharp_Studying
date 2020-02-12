f87a3a65edfe080bc535451cf718697ab1f34b86
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-12 07:32:38 -0800

    2020_02_12

---
+++ BoxingAndUnboxingWithUpcastingAndDownCasting/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxingAndUnboxingWithUpcastingAndDownCasting
+{
+    class A
+    {
+        public void PrintA()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+
+    class B : A
+    {
+        public void PrintB()
+        {
+            Console.WriteLine("World");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            B Test1 = new B();
+            object obj = Test1;
+            A Test2 = (A)obj; //업캐스팅 다운캐스팅
+            Test2.PrintA();
+            Test1.PrintB();
+        }
+    }
+}
