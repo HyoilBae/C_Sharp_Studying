eca9a86168700f71a46ed639b3b2ec3fae9615ce
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-12 07:31:12 -0800

    2020_02_12

---
+++ anonymous_type/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anonymous_type
+{
+    delegate int Mydelegate(int a, int b);
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            var anony = new { age = 11, name = "ABC" };
+
+            Console.WriteLine("Age: {0}, Name: {1}", anony.age, anony.name);
+
+            var anonyArray = new
+            {
+                Int = new int[] { 1, 2, 3, 4, 5 },
+                Float = new float[] { 1.1f, 2.2f, 3.3f, 4.4f }
+            };
+
+            foreach (var element in anonyArray.Int)
+            {
+                Console.WriteLine("{0}", element);
+            }
+
+            foreach (var element in anonyArray.Float)
+            {
+                Console.WriteLine("{0}", element);
+            }
+
+
+
+            Mydelegate add;
+            add = delegate (int a, int b)
+            {
+                return a + b;
+            };
+            Console.WriteLine(add(11, 22));
+        }
+    }
+}
