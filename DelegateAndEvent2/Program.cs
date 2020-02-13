3c934e9291442ffe82d07646a9caf816aa938d13
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-13 08:59:29 -0800

    2020_02_13

---
+++ DelegateAndEvent2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateAndEvent2
+{
+    delegate void DelegateType();
+
+    class A
+    {
+        public void PrintA()
+        {
+            Console.WriteLine("PrintA");
+        }
+
+        public void PrintB()
+        {
+            Console.WriteLine("PrintB");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A Test = new A();
+            DelegateType DelFunc = Test.PrintA;
+            DelFunc += Test.PrintB;
+            DelFunc();
+            DelFunc -= Test.PrintB;
+            DelFunc();
+        }
+    }
+}
