bd8b047e9153e72f9f6a75e9beb6fef370db3280
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-13 08:58:44 -0800

    2020_02_13

---
+++ delegate_chain/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegate_chain
+{
+    delegate void MyDelegate();
+    class Program
+    {
+        public static void method1() { Console.WriteLine("Method1"); }
+        public static void method2() { Console.WriteLine("Method2"); }
+        public static void method3() { Console.WriteLine("Method3"); }
+
+        static void Main(string[] args)
+        {
+            MyDelegate del1 = new MyDelegate(method1);
+            del1 += method2;
+            del1 += method3;
+
+            del1();
+
+            del1 -= method2;
+            del1 -= method3;
+
+            del1();
+        }
+    }
+}
