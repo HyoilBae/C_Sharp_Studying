dee5264f37d2dd8ace990b2a9c70914b1a626a5e
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-13 09:00:51 -0800

    2020_02_13

---
+++ event_delegate/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace event_delegate
+{
+    delegate void MyDelegate(int a);
+
+    class EventManager
+    {
+        public event MyDelegate eventCall;
+
+        public void NumberCheck(int num)
+        {
+            if (num%2 == 0)
+            {
+                eventCall(num);
+            }
+        }
+    }
+    class Program
+    {
+        static void EvenNumber (int num)
+        {
+            Console.WriteLine("{0} is even number", num);
+        }
+        static void Main(string[] args)
+        {
+            EventManager eventManager = new EventManager();
+            eventManager.eventCall += new MyDelegate(EvenNumber);
+
+            for (int i = 0; i < 10; i++)
+            {
+                eventManager.NumberCheck(i);
+            }
+
+        }
+    }
+}
