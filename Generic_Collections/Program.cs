2a7e7b64f49cd27b2cf18ccb50c9a23b39994dd1
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-15 09:44:54 -0800

    2020_02_15

---
+++ Generic_Collections/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list1 = new List<int>();
+
+            list1.Add(11);
+            list1.Add(22);
+            list1.Add(33);
+            list1.RemoveAt(1);
+            list1.Remove(11);
+            list1.Insert(0, 44);
+
+            foreach (int element in list1)
+            {
+                Console.WriteLine("{0} ", element);
+            }
+
+            List<string> list2 = new List<string>();
+            list2.Add("A");
+            list2.Add("B");
+            list2.Add("C");
+            list2.RemoveAt(0);
+            list2.Remove("B");
+            list2.Insert(1, "D");
+
+            foreach (string element in list2)
+            {
+                Console.WriteLine("{0} ", element);
+            }
+        }
+    }
+}
