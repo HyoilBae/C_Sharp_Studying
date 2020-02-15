10bad6c19dde2473dd55daecf93421b87d8ef2b6
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-15 09:45:21 -0800

    2020_02_15

---
+++ Generic_Collections4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Generic_Collections4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> d1 = new Dictionary<string, string>();
+
+            d1["Banana"] = "바나나";
+            d1["Apple"] = "사과";
+
+            Console.WriteLine(d1["Banana"]);
+            Console.WriteLine(d1["Apple"]);
+        }
+    }
+}
