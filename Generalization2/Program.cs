af3a12bb54fb8407562d718f8ff5939ed0087c1f
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-15 09:44:35 -0800

    2020_02_15

---
+++ Generalization2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Generalization2
+{
+    class List <T>
+    {
+        public T[] arr;
+
+        public List() { arr = new T[1]; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list1 = new List<int>();
+            list1.arr[0] = 10;
+            List<float> list2 = new List<float>();
+            list2.arr[0] = 2.2f;
+            List<string> list3 = new List<string>();
+            list3.arr[0] = "ABC";
+
+            Console.WriteLine(list1.arr[0]);
+            Console.WriteLine(list2.arr[0]);
+            Console.WriteLine(list3.arr[0]);
+        }
+    }
+}
