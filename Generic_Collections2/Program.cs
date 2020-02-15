dc64656eb2bde3a7d7e315ca3cd62e6a822d1f73
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-15 09:45:03 -0800

    2020_02_15

---
+++ Generic_Collections2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Collections2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> que1 = new Queue<int>();
+
+            que1.Enqueue(11);
+            que1.Enqueue(22);
+            que1.Enqueue(33);
+
+            while (que1.Count > 0)
+            {
+                Console.WriteLine("{0} ", que1.Dequeue());
+            }
+
+            Queue<string> que2 = new Queue<string>();
+
+            que2.Enqueue("ABC");
+            que2.Enqueue("DEF");
+
+            while (que2.Count > 0)
+            {
+                Console.WriteLine("{0} ", que2.Dequeue());
+            }
+
+        }
+    }
+}
