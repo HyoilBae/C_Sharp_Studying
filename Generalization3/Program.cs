9fc80c0ba737cc44b3a271dd851c79073d30e0e3
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-15 09:44:45 -0800

    2020_02_15

---
+++ Generalization3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generalization3
+{
+    class Parent
+    {
+        public string name { set; get; }
+
+        public Parent() { name = "Parent Class";  }
+    }
+    class Children : Parent
+    {
+        public Children() { name = "Children Class"; }
+    }
+
+    class List<T> where T : Parent
+    {
+        public T[] array;
+
+        public List() { array = new T[2]; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Parent> list = new List<Parent>();
+
+            list.array[0] = new Parent();
+            list.array[1] = new Children();
+
+            Console.WriteLine(list.array[0].name);
+            Console.WriteLine(list.array[1].name);
+        }
+    }
+}
