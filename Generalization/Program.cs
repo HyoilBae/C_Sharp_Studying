d337a90e7775c6c631b07d684b6c12276a7e1625
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-15 09:44:23 -0800

    2020_02_15

---
+++ Generalization/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generalization
+{
+    class Program
+    {
+        static void print <T> (T value)
+        {
+            Console.WriteLine(value);
+        }
+        static void Main(string[] args)
+        {
+            int age = 29;
+            float height = 178.1f;
+            string name = "Mr. W";
+
+            print<int>(age);
+            print<float>(height);
+            print<string>(name);
+        }
+    }
+}
