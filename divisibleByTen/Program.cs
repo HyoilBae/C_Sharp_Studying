962205ceb84b069a20fd41e54081b8aff1ac15af
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-15 09:44:03 -0800

    2020_02_15

---
+++ divisibleByTen/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace divisibleByTen
+{
+    class Solution
+    {
+        public int DivideByTen(int[] nums)
+        {
+            int count = 0;
+            foreach (int num in nums)
+            {
+                if (num%10 == 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+          
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            int[] test = new int[]{ 20, 25, 30, 35, 40 };
+
+            Console.WriteLine(s.DivideByTen(test));
+        }
+    }
+}
