408f6074fa6f4ea45bf00b489c65d9242e966e93
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-16 10:43:11 -0800

    2020_02_16

---
+++ Inheritance/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    //Public, Protected, Private Class
+    class A
+    {
+        int number;
+        protected string name = "Hello";
+        public A (int num)
+        {
+            number = num;
+        }
+
+        public void PrintA()
+        {
+            Console.WriteLine(number);
+        }
+    }
+
+    class B : A
+    {
+        string name = "World";
+        public B(int num): base(num)
+        {
+
+        }
+
+        public void PrintB()
+        {
+            Console.WriteLine("{0} {1}", base.name, name);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            B Test = new B(3);
+            Test.PrintA();
+            Test.PrintB();
+        }
+    }
+}
