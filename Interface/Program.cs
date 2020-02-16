378849839f0832e3dcabd896fe7bfe0b5cf53492
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-16 10:43:48 -0800

    2020_02_16

---
+++ Interface/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    interface IA
+    {
+        void PrintA();
+    }
+
+   interface IB
+    {
+        void PrintB();
+    }
+
+    class C : IA, IB
+    {
+        public void PrintA()
+        {
+            Console.WriteLine("interface A");
+        }
+        public void PrintB()
+        {
+            Console.WriteLine("interface B");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            C Test = new C();
+            Test.PrintA();
+            Test.PrintB();
+        }
+    }
+}
