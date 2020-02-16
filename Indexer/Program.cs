8c79028a93d30e413e92bcd1f544d66d0832e811
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-16 10:42:53 -0800

    2020_02_16

---
+++ Indexer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexer
+{
+    class A
+    {
+        private int[] number = new int[5];
+        public int this[int index]
+        {
+            get { return number[index]; }
+            set { number[index] = value; }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A test = new A();
+            for (int i = 0; i < 5; i++)
+            {
+                test[i] = i;
+                Console.WriteLine(test[i]);
+            }
+        }
+    }
+}
