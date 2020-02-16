fd8c52cd272fe97667de63be9b25fbca54e00265
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-16 10:43:29 -0800

    2020_02_16

---
+++ Inheritance2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance2
+{
+    abstract class A
+    {
+        int number = 3;
+        public abstract void Print();
+        public void PrintNumber()
+        {
+            Console.WriteLine(number);
+        }
+    }
+
+    class B : A
+    {
+        public override void Print()
+        {
+            Console.WriteLine("abstract class");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            B Test = new B();
+            Test.Print();
+            Test.PrintNumber();
+        }
+    }
+}
