44367fae544f9ca0f87d0afaeb08e7b2c51b1d2a
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-17 10:40:17 -0800

    2020_02_17

---
+++ reverseString/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace reverseString
+{
+    public class Solution
+    {
+        public void ReverseString(char[] s)
+        {
+            ////##########Solution#1###############
+            //for (int i = 0; i < s.Length/2; i++)
+            //{
+            //    var tmp = s[i];
+            //    s[i] = s[s.Length - (i + 1)];
+            //    s[s.Length - (i + 1)] = tmp;
+            //}
+            //Console.WriteLine(s);
+
+            //Solution#2
+            int left = 0, right = s.Length - 1;
+            while (left < right)
+            {
+                char tmp = s[left];
+                s[left++] = s[right];
+                s[right--] = tmp;
+            }
+            Console.WriteLine(s);
+
+            //####This works but I don't know why LeetCode makes errors?
+            //for (int i = 0; i < s.Length; i++)
+            //{
+            //    int j = s.Length - (i + 1);
+            //    Console.Write(s[j]);
+            //}
+            //Console.WriteLine("");
+
+            //Console.WriteLine("foreach loop");
+            //foreach (char element in s)
+            //{
+            //    Console.Write(element);
+            //}
+
+            //Console.WriteLine("for loop");
+            //for (int i = 0; i < s.Length; i++)
+            //{
+            //    Console.WriteLine($"char[] s char{(i)} = {s[i]}");
+            //}
+
+
+            //char[] reverseS = Array.Reverse(s);
+            //Console.WriteLine(reverseS);
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+            //char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            //solution.ReverseString(alphabet);
+
+            Console.WriteLine("Enter : ");
+            string userInput = Console.ReadLine();
+            //Console.WriteLine(userInput.GetType());
+            char[] userInputChar = userInput.ToCharArray();
+            solution.ReverseString(userInputChar);
+
+            //Console.WriteLine(alphabet);
+            //Array.Reverse(alphabet);
+            //Console.WriteLine(alphabet);
+            //Console.WriteLine(alphabet.GetType());
+
+
+            //Console.WriteLine("Enter a string");
+            //string userString = (Console.ReadLine()).ToLower();
+            //char[] userStringChar = userString.ToCharArray();
+            //char[] reverseString = new char[userStringChar.Length];
+            //for (int i = 0; i < userStringChar.Length; i++)
+            //{
+
+            //}
+
+        }
+    }
+}
---
+++ reverseString/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("reverseString")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("reverseString")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1b3b4427-5389-43b2-85eb-64d7df0423a9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
