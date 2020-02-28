d4328ebc31c92bd7fc2026b50ff0a2e19aaa9959
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-27 16:51:02 -0800

    Update MultiDimensionalArrays.cs

--- MultiDimensionalArrays.cs
+++ MultiDimensionalArrays.cs
@@ -11,6 +11,7 @@ namespace MultiDimensionalArrays
         static void Main(string[] args)
         {
             //Single-Dimensional Array Example
+            Console.WriteLine("-------------Single Array Example-----------");
             int[] scoreArr = new int[] { 60, 50, 90, 80, 30 };
             int sum = 0;
             for (int i = 0; i < scoreArr.Length; i++)
@@ -19,8 +20,10 @@ namespace MultiDimensionalArrays
             }
             float average = sum / 5;
             Console.WriteLine("Average : {0}", average);
+            Console.WriteLine();
 
             //2D Array
+            Console.WriteLine("-------------2D Array Example-----------");
             int[,] arrScore = new int[2, 5] { { 90, 80, 70, 42, 30},
                                               { 24, 50, 100, 52, 70 } };
             int[] arrSum = new int[2] { 0, 0 };
@@ -36,8 +39,38 @@ namespace MultiDimensionalArrays
                 average = arrSum[i] / 5;
                 Console.WriteLine("Average[" +i+ "] : " + average);
             }
+            Console.WriteLine();
+
+            //3D Array
+            Console.WriteLine("-------------3D Array Example-----------");
+            int[,,] _3Darray = new int[4, 3, 2]
+            {
+                {{1,2}, {3,4}, {5,6}},
+                {{7,8}, {9,10}, {11,12}},
+                {{13, 14 }, {15,16}, {17,18}},
+                {{19,20}, {21,22}, {23,24}}
+            };
+            for (int i = 0; i < _3Darray.GetLength(0); i++)
+            {
+                for (int j = 0; j < _3Darray.GetLength(1); j++)
+                {
+                    Console.Write("{ ");
+                    for (int k = 0; k < _3Darray.GetLength(2); k++)
+                    {
+                        Console.Write("{0} ", _3Darray[i, j, k]);
+                    }
+                    Console.Write("} ");
+                }
+                Console.WriteLine();
+            }
+            foreach (var item in _3Darray)
+            {
+                Console.Write(" {0} ", item);
+            }
+            Console.WriteLine();
 
             //Jagged Array
+            Console.WriteLine("-------------Jagged Array Example-----------");
             int[][] jaggedArr = new int[3][]
             {
                 new int[] {31, 32, 33 },
