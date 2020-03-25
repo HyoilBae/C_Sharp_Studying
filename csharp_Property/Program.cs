29a74c6e96bdebc9af41bdbf63b0f5167f6e3b7b
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-25 06:56:49 -0700

    Property

---
+++ csharp_Property/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_Property
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("2019-0000G1", "Park", 20, "Baseball");
+            Console.WriteLine($"student1.GetId()\t: {student1.GetId()}");
+            Console.WriteLine($"student1.GetName()\t: {student1.GetName()}");
+            Console.WriteLine($"student1.GetAge()\t: {student1.GetAge()}");
+            Console.WriteLine($"student1.GetMajor()\t: {student1.GetMajor()}");
+        }
+    }
+}
---
+++ csharp_Property/Student.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_Property
+{
+    class Student
+    {
+        private string id;
+        private string name;
+        private int age;
+        private string major;
+
+        public Student()
+        {
+            Console.WriteLine("Student Default Constructor");
+        }
+
+        public Student(string id, string name, int age, string major)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+            this.major = major;
+        }
+
+        public string GetId()
+        {
+            return id;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public int GetAge()
+        {
+            return age;
+        }
+
+        public string GetMajor()
+        {
+            return major;
+        }
+
+        public void SetId(string id)
+        {
+            this.id = id;
+        }
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public void SetAge(int age)
+        {
+            this.age = age;
+        }
+
+        public void SetMajor(string major)
+        {
+            this.major = major;
+        }
+    }
+}
