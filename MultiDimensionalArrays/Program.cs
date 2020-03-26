cfb1c5fbec598ef7561ce162c1a4b6273a354052
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-26 08:27:42 -0700

    MultiDimensionalArray

---
+++ MultiDimensionalArrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiDimensionalArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Single-Dimensional Array Example
+            int[] scoreArr = new int[] { 60, 50, 90, 80, 30 };
+            int sum = 0;
+            for (int i = 0; i < scoreArr.Length; i++)
+            {
+                sum += scoreArr[i];
+            }
+            float average = sum / 5;
+            Console.WriteLine("Average : {0}", average);
+
+
+        }
+    }
+}
