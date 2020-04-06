da8225b3249789dac0b8756fcf20499f3176696f
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-04-06 07:43:46 -0700

    RandomAverages

---
+++ RandomAverages/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace RandomAverages
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.NumberOfValues = new System.Windows.Forms.Label();
+            this.CalculateRandomAverages = new System.Windows.Forms.Button();
+            this.RandomAverageResults = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(220, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(322, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // NumberOfValues
+            // 
+            this.NumberOfValues.AutoSize = true;
+            this.NumberOfValues.Location = new System.Drawing.Point(74, 70);
+            this.NumberOfValues.Name = "NumberOfValues";
+            this.NumberOfValues.Size = new System.Drawing.Size(140, 20);
+            this.NumberOfValues.TabIndex = 1;
+            this.NumberOfValues.Text = "Number of values: ";
+            this.NumberOfValues.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // CalculateRandomAverages
+            // 
+            this.CalculateRandomAverages.Location = new System.Drawing.Point(559, 67);
+            this.CalculateRandomAverages.Name = "CalculateRandomAverages";
+            this.CalculateRandomAverages.Size = new System.Drawing.Size(75, 43);
+            this.CalculateRandomAverages.TabIndex = 2;
+            this.CalculateRandomAverages.Text = "Start";
+            this.CalculateRandomAverages.UseVisualStyleBackColor = true;
+            this.CalculateRandomAverages.Click += CalculateRandomAveragesAsync_Click;
+            // 
+            // RandomAverageResults
+            // 
+            this.RandomAverageResults.AutoSize = true;
+            this.RandomAverageResults.Location = new System.Drawing.Point(293, 117);
+            this.RandomAverageResults.Name = "RandomAverageResults";
+            this.RandomAverageResults.Size = new System.Drawing.Size(51, 20);
+            this.RandomAverageResults.TabIndex = 4;
+            this.RandomAverageResults.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.RandomAverageResults);
+            this.Controls.Add(this.CalculateRandomAverages);
+            this.Controls.Add(this.NumberOfValues);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Random Averages";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label NumberOfValues;
+        private System.Windows.Forms.Button CalculateRandomAverages;
+        private System.Windows.Forms.Label RandomAverageResults;
+    }
+}
+
---
+++ RandomAverages/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomAverages
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private double computeAverages(long noOfValues)
+        {
+            double total = 0;
+            Random rand = new Random();
+
+            for (long values = 0; values < noOfValues; values++)
+            {
+                total += rand.NextDouble();
+            }
+
+            return total / noOfValues;
+        }
+
+        private void CalculateRandomAverages_Click(object sender, EventArgs e)
+        {
+            long noOfValues = 0;
+            long.TryParse(textBox1.Text, out noOfValues);
+            RandomAverageResults.Text = computeAverages(noOfValues).ToString();
+        }
+        
+        private async void CalculateRandomAveragesAsync_Click(object sender, EventArgs e)
+        {
+            RandomAverageResults.Text = "Calculating";
+            long noOfValues = 0;
+            long.TryParse(textBox1.Text, out noOfValues);
+            double result = await asyncComputeAverages(noOfValues);
+            RandomAverageResults.Text = result.ToString();
+        }
+
+        private Task<double> asyncComputeAverages(long noOfValues)
+        {
+            return Task<double>.Run(() => 
+                computeAverages(noOfValues)
+            );
+        }
+    }
+}
