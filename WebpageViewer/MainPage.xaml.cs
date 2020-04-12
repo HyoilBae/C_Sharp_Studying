4c26c0556fcccfe55e8253f7ac693010d7310808
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-04-12 14:26:04 -0700

    WebPageViewer

---
+++ WebpageViewer/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WebpageViewer
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void LoadWebsiteButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ResultTextBlock.Text = await FetchWebPage(URLTextBox.Text);
+                StatusTextBlock.Text = "Page Loaded";
+            }
+            catch (Exception ex)
+            {
+                StatusTextBlock.Text = ex.Message;
+            }
+        }
+
+        private async Task<string> FetchWebPage(string url)
+        {
+            HttpClient httpClient = new HttpClient();
+            return await httpClient.GetStringAsync(url);
+        }
+    }
+}
