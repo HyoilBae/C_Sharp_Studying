9938de146f6ae74a5b3600957a0c8e64278244f6
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-04-12 14:27:34 -0700

    WebApplication

---
+++ WebApplication/Classes/MyRecord.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Classes
+{
+    public class MyRecord
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ WebApplication/WebForm.aspx.cs
@@ -0,0 +1,28 @@
+﻿using WebApplication.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication
+{
+    public partial class WebForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            List<MyRecord> myRecordList = new List<MyRecord>();
+
+            myRecordList.Add(new MyRecord() { Id = 1, Name = "Kim" });
+            myRecordList.Add(new MyRecord() { Id = 2, Name = "Travis" });
+            myRecordList.Add(new MyRecord() { Id = 3, Name = "Christian" });
+            myRecordList.Add(new MyRecord() { Id = 4, Name = "Maddie" });
+
+            DropDownList1.DataSource = myRecordList;
+            DropDownList1.DataTextField = "Name";
+            DropDownList1.DataValueField = "Id";
+            DropDownList1.DataBind();
+        }
+    }
+}
